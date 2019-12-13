28e9c8d74262b1edaf7ca25a72f7544d11552cc0
cartercheng34 <j3361159@gmail.com>
2019-12-13 18:21:10 -0500

    support multi thread

--- src/scene.cpp
+++ src/scene.cpp
@@ -182,97 +182,97 @@ Image3f Scene::raytrace() const
 
     // Hint: you can create a Progress object (progress.h) to provide a 
     // progress bar during rendering.
-    {
-        auto clean_integrator = m_integrator;
-        for (int count = 0 ; count < m_integrator->iter ; count++){
+    // {
+    //     auto clean_integrator = m_integrator;
+    //     for (int count = 0 ; count < m_integrator->iter ; count++){
 
-            auto tmp_integrator = clean_integrator;
-            tmp_integrator->preprocess(*this, count);
-            Progress progress("Rendering", image.size());
-            // Generate a ray for each pixel in the ray image
-            for (auto y : range(image.height()))
-            {
-                for (auto x : range(image.width()))
-                {
-                    INCREMENT_TRACED_RAYS;
-                    
-                    Color3f total = Color3f(0.f);
-                    
-                    for (int i = 0; i < m_imageSamples; i++){
-                        auto ray = m_camera->generateRay(x + randf(), y + randf());
-                        Color3f init;
-                        if (tmp_integrator){
-                            init = tmp_integrator->Li(*this, ray, 0);
-                            
-                        }
-                        else Color3f init = recursiveColor(ray, 0);
-
-                        total += init;
-                    }
-                    total /= m_imageSamples;
-                    
-                    Color3f color = total;
-
-                    // TODO: Call recursiveColorFunction ``NumSamples'' times and average the
-                    // results. Assign the average color to ``color''
-
-                    image(x, y) += color / m_integrator->iter;
-                    ++progress;
-                }
-            }
-        }
-        
-	}	
-
-
-    // const int k_threads = std::thread::hardware_concurrency();
-    // std::vector<std::thread> threads(k_threads);
-    // for (int t = 0; t < k_threads; ++t) {
-    //     threads[t] = std::thread(std::bind([&](int start, int end, int t) {
-    //         auto clean_integrator = m_integrator;
-    //         for (int count = 0 ; count < m_integrator->iter ; count++){
-
-    //             auto tmp_integrator = clean_integrator;
-    //             tmp_integrator->preprocess(*this, count);
-    //             Progress progress("Rendering", image.size());
-    //             // Generate a ray for each pixel in the ray image
-    //             // for (auto y : range(image.height()))
-    //             for(int y = start ; y < end ; y++)
+    //         auto tmp_integrator = clean_integrator;
+    //         tmp_integrator->preprocess(*this, count);
+    //         Progress progress("Rendering", image.size());
+    //         // Generate a ray for each pixel in the ray image
+    //         for (auto y : range(image.height()))
+    //         {
+    //             for (auto x : range(image.width()))
     //             {
-    //                 for (auto x : range(image.width()))
-    //                 {
-    //                     INCREMENT_TRACED_RAYS;
-                        
-    //                     Color3f total = Color3f(0.f);
-                        
-    //                     for (int i = 0; i < m_imageSamples; i++){
-    //                         auto ray = m_camera->generateRay(x + randf(), y + randf());
-    //                         Color3f init;
-    //                         if (tmp_integrator){
-    //                             init = tmp_integrator->Li(*this, ray, 0);
-                                
-    //                         }
-    //                         else Color3f init = recursiveColor(ray, 0);
-
-    //                         total += init;
+    //                 INCREMENT_TRACED_RAYS;
+                    
+    //                 Color3f total = Color3f(0.f);
+                    
+    //                 for (int i = 0; i < m_imageSamples; i++){
+    //                     auto ray = m_camera->generateRay(x + randf(), y + randf());
+    //                     Color3f init;
+    //                     if (tmp_integrator){
+    //                         init = tmp_integrator->Li(*this, ray, 0);
+                            
     //                     }
-    //                     total /= m_imageSamples;
-                        
-    //                     Color3f color = total;
+    //                     else Color3f init = recursiveColor(ray, 0);
 
-    //                     // TODO: Call recursiveColorFunction ``NumSamples'' times and average the
-    //                     // results. Assign the average color to ``color''
-
-    //                     image(x, y) += color / m_integrator->iter;
-    //                     ++progress;
+    //                     total += init;
     //                 }
+    //                 total /= m_imageSamples;
+                    
+    //                 Color3f color = total;
+
+    //                 // TODO: Call recursiveColorFunction ``NumSamples'' times and average the
+    //                 // results. Assign the average color to ``color''
+
+    //                 image(x, y) += color / m_integrator->iter;
+    //                 ++progress;
     //             }
     //         }
-    //     }, t*image.height()/k_threads, (t+1)==k_threads ? image.height() : (t+1)*image.height()/k_threads, t));
-    // }
-    // for (int t = 0; t < k_threads; ++t) {
-    //     threads[t].join();
-    // }
+    //     }
+        
+	// }	
+
+
+    const int k_threads = std::thread::hardware_concurrency();
+    std::vector<std::thread> threads(k_threads);
+    for (int t = 0; t < k_threads; ++t) {
+        threads[t] = std::thread(std::bind([&](int start, int end, int t) {
+            auto clean_integrator = m_integrator;
+            for (int count = 0 ; count < m_integrator->iter ; count++){
+
+                auto tmp_integrator = clean_integrator;
+                tmp_integrator->preprocess(*this, count);
+                Progress progress("Rendering", image.size());
+                // Generate a ray for each pixel in the ray image
+                // for (auto y : range(image.height()))
+                for(int y = start ; y < end ; y++)
+                {
+                    for (auto x : range(image.width()))
+                    {
+                        INCREMENT_TRACED_RAYS;
+                        
+                        Color3f total = Color3f(0.f);
+                        
+                        for (int i = 0; i < m_imageSamples; i++){
+                            auto ray = m_camera->generateRay(x + randf(), y + randf());
+                            Color3f init;
+                            if (tmp_integrator){
+                                init = tmp_integrator->Li(*this, ray, 0);
+                                
+                            }
+                            else Color3f init = recursiveColor(ray, 0);
+
+                            total += init;
+                        }
+                        total /= m_imageSamples;
+                        
+                        Color3f color = total;
+
+                        // TODO: Call recursiveColorFunction ``NumSamples'' times and average the
+                        // results. Assign the average color to ``color''
+
+                        image(x, y) += color / m_integrator->iter;
+                        ++progress;
+                    }
+                }
+            }
+        }, t*image.height()/k_threads, (t+1)==k_threads ? image.height() : (t+1)*image.height()/k_threads, t));
+    }
+    for (int t = 0; t < k_threads; ++t) {
+        threads[t].join();
+    }
 
     auto timeSpan = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - fulltime);
     int frameTimeMs = static_cast<int>(timeSpan.count());
